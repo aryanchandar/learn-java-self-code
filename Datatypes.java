77b6a2b651957bb052eb9d17037a0b14e292a59e
Aryan Chandra <71782395+aryanchandar@users.noreply.github.com>
2022-05-21 16:36:12 +0530

    10change to40

--- Datatypes.java
+++ Datatypes.java
@@ -1,6 +1,6 @@
 public class Datatypes{
     public static void main (String[]args){
-        int age = 10;
+        int age = 40;
         //float rateOfInterest =2.3f;
        // double rate =4.544232;
         System.out.println(age);
